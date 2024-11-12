bf22acf994250a531f0bafeb797e05a0aecf9b5e
yeonjiAn <yeonji.an@concentrix.com>
2024-11-12 18:01:34 +0900

    [2024-11-11] 작업 - 2

--- src/main/frontend/src/page/list/listPage.jsx
+++ src/main/frontend/src/page/list/listPage.jsx
@@ -1,37 +1,48 @@
 import axios from 'axios';
+import {useState} from "react";
 
 const ListPage = () => {
-    //함수 정의
-    const CreateUserBtn = (e) => {
-       alert("답 : "+e);
-       axios.post("/createUser",{
-        //api 통신해야됨
-        })
-    };
+    //함수 정의 :: getter / setter 같은.
+    const [user_name, setUser_name] = useState();
+    const [user_age , setUser_age] = useState();
+
+    async function createUser(e){
+        e.preventDefault(); // 동작제어
+        const formData = new FormData(document.getElementById('createForm'));
+        await axios.post({
+            method : 'post',
+            url : "http://localhost:8080/createUser",
+            withCredentials : true,
+            data : formData,
+            headers: {'Content-type': 'application/json'}
+        }).then(function(response){
+            console.log("성공");
+        });
+    }
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
                 <div className="mt-6 grid grid-cols-4 gap-6">
-                    <form action="#" method="POST" id="createUser"
+                    <form id="createForm" name="createForm"
                           className="col-span-2 bg-white shadow-xl rounded px-8 pt-6 pb-6 mb-4 border border-gray-200"> {/* shadow-xl을 사용하여 더 진한 그림자 */}
                         <h3 className="text-center text-2xl font-bold tracking-tight text-gray-900 mb-6">사용자 등록</h3>
 
                         <div className="mb-4 flex flex-col items-center mt-12">
                             <input
                                 className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="username" type="text" placeholder="이름"/>
+                                id="name" name="name" type="text" placeholder="이름"/>
                             <p className="text-red-500 text-xs italic">이름을 필수로 작성해주세요.</p>
                         </div>
 
                         <div className="mb-4 flex flex-col items-center mt-6">
                             <input
                                 className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="age" type="text" placeholder="나이"/>
+                                id="age" name="age" type="text" placeholder="나이"/>
                         </div>
 
                         <div className="flex items-center justify-center mt-8">
                             <button
-                                onSubmit={CreateUserBtn}
+                                onClick={createUser}
                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                                 type="submit">
                                 저장
--- src/main/java/com/group/library_project/controller/user/UserController.java
+++ src/main/java/com/group/library_project/controller/user/UserController.java
@@ -3,35 +3,35 @@ package com.group.library_project.controller.user;
 import com.group.library_project.domain.User.User;
 import com.group.library_project.dto.user.request.UserCreateRequest;
 import com.group.library_project.dto.user.response.UserResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class UserController {
 
     private List<User> users = new ArrayList<>();
 
-    @GetMapping("/getaa")
+    @GetMapping("/api/getaa")
     public String getaa(){
         return " 테스트";
     }
 
-    @PostMapping("/createUser")
+    @PostMapping("/api/createUser")
     public void createUser(@RequestBody UserCreateRequest request){
         users.add(new User(request.getName(),request.getAge()));
+        log.info("request 값 : ( {} , {} )",request.getAge(),request.getName()); // log.info 를 사용할때 매개변수화된 로그메시지를 사용해주는게 좋다. gradle 에 annotationProcessor 추가해줘야함 롬복 컴파일 에러 방지
     }
 
-    @GetMapping("/user")
-    public List<UserResponse> getUsers(){
+    @GetMapping("/api/user")
+    public List<UserResponse> getUsers() {
         List<UserResponse> reponses = new ArrayList<>();
-        for(int i = 0; i < users.size(); i++){
-            reponses.add(new UserResponse(i + 1, users.get(i) ));
+        for (int i = 0; i < users.size(); i++) {
+            reponses.add(new UserResponse(i + 1, users.get(i)));
         }
         return reponses;
     }
-
-
-
 }
